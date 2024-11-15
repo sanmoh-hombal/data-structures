d134d53490e8092ee5d451cb34d99668f0a45f11
Sanmoh Hombal <sanmoh.hombal@gmail.com>
2024-11-15 12:12:49 +0530

    ✨ feat(Arrays): Add brute-force method to find the largest element in an array
    Added a brute-force approach to find the largest element in an array. Included annotations from JetBrains for contract and nullability checks.

--- src/main/java/org/sanmoh/Arrays/LargestElementInAnArray.java
+++ src/main/java/org/sanmoh/Arrays/LargestElementInAnArray.java
@@ -1,5 +1,8 @@
 package org.sanmoh.Arrays;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Largest Element in an Array
  *
@@ -15,4 +18,20 @@ package org.sanmoh.Arrays;
  * <pre>
  *    20
  */
-public class LargestElementInAnArray {}
+public class LargestElementInAnArray {
+  /**
+   * Finds the largest element in the given array using a brute-force approach.
+   *
+   * @param array the array of integers in which the largest element is to be found
+   * @return the largest element in the provided array
+   */
+  @Contract(pure = true)
+  public static Integer BruteForce(Integer @NotNull [] array) {
+    // Initialize the largest element as the first element of the array
+    Integer largest = array[0];
+    // Iterate through the array and update the largest element if a larger element is found
+    for (Integer integer : array) if (integer > largest) largest = integer;
+    // Return the largest element
+    return largest;
+  }
+}
